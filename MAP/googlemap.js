86b68ed325efa296904a8d37bce0c30786657e73
Szász Szilárd <szasz.szilard7@gmail.com>
2016-11-29 00:30:37 +0200

    automatic route draw by user input

--- MAP/googlemap.js
+++ MAP/googlemap.js
@@ -13,13 +13,11 @@ var aghires = {lat: 47.1640112, lng: 23.004459};
 	//inicializálja a térképet
 	function initMap() {
 		directionsService = new google.maps.DirectionsService;
-		directionsDisplay = new google.maps.DirectionsRenderer;
 		var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 8,
 			center: sarmasag
 		}
 	);
-	directionsDisplay.setMap(map);
 	
 	function calculateAndDisplayRoute(x,y) {
 		directionsService.route({
@@ -28,11 +26,15 @@ var aghires = {lat: 47.1640112, lng: 23.004459};
 			travelMode: 'DRIVING'
 		}, function(response, status) {
 			if (status === 'OK') {
+				directionsDisplay = new google.maps.DirectionsRenderer;
+				directionsDisplay.setMap(map);
 				directionsDisplay.setDirections(response);
+
 				//responses[responseIndex] = response;
+				//responseIndex++;
+				//directionsDisplay.setDirections(responses[i]);
 				//console.log(response.routes[0].legs[0].distance.value);
 				//directionsDisplay.setDirections(responses[responseIndex]);
-				//responseIndex++;
 				//console.log(response);
 			} else {
 				window.alert('Directions request failed due to ' + status);
@@ -40,14 +42,20 @@ var aghires = {lat: 47.1640112, lng: 23.004459};
         });
     }
 	
-	calculateAndDisplayRoute(sarmasag, cluj);
-	calculateAndDisplayRoute(sarmasag, aghires);
-	calculateAndDisplayRoute(aghires, cluj);
+	//directionsService.route(requestArray[i], directionResults);
 	
+	//addMarker(sarmasag);
+	//addMarker(aghires);
+	//addMarker(cluj);
 	
-	//for (var i = 0; i < responseIndex; i++){
-	//	directionsDisplay.setDirections(responses[0]);
-	//}
+	console.log(markers);
+	
+	//calculateAndDisplayRoute(markers[0].getPosition(), markers[1].getPosition());
+	//calculateAndDisplayRoute(markers[1].getPosition(), markers[2].getPosition());
+	
+	//renderArray[cur] = new google.maps.DirectionsRenderer();
+    //renderArray[cur].setMap(map);
+    //renderArray[cur].setDirections(result);
 	
 	function addMarker(x){
 		markers[markerLabelNum] = new google.maps.Marker({
@@ -70,6 +78,11 @@ var aghires = {lat: 47.1640112, lng: 23.004459};
 		var lngX = event.latLng.lng();
 		var x = {lat: latX, lng: lngX};
 		addMarker(x);
+		
+		if (markerLabelNum > 1){
+			for (var i = 0; i < markerLabelNum-1; i++)
+				calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum-1].getPosition());
+		}
 	});
 
 
