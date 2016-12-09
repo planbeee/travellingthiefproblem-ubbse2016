c67b1ecbeb8f772a60206ed666b6e43a0853eb47
Szasz Imre <sz.lenard94@gmail.com>
2016-12-09 04:29:11 +0200

    Restructured code, started on geocoding.

--- MAP/googlemap.js
+++ MAP/googlemap.js
@@ -3,26 +3,49 @@ var markerLabelNum = 0;
 var responseIndex = 0;
 var responses = [];
 var myData = [];
-var wow = ["green", "purple", "red", "pink", "yellow", "blue", "orange", "white", "black", "brown"];
+var wow = ["green", "purple", "red", "yellow", "blue", "orange", "black", "brown"];
+var colorIndex = 0;
 
 var latlngCenter = { lat: 45.7820031, lng: 25.0681987 };
-var cluj = { lat: 46.7833643, lng: 23.5464732 };
-var aghires = { lat: 47.1640112, lng: 23.004459 };
 
 var directionsService;
 var directionsDisplay;
+var map;
 
 //inicializálja a térképet
 function initMap() {
     directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
         center: latlngCenter
     });
+
+
+    // purpose: decode latitude and longitude to address
+    var geocoder = new google.maps.Geocoder;
+
+    google.maps.event.addListener(map, "click", function(event) {
+        routeDisplay(map, geocoder, event);
+    })
 }
 
-// purpose: decode latitude and longitude to address
-var geocoder = new google.maps.Geocoder;
+function routeDisplay(map, geocoder, event) {
+    var latX = event.latLng.lat();
+    var lngX = event.latLng.lng();
+    var latlngX = { lat: latX, lng: lngX };
+    addMarker(latlngX);
+
+    if (markerLabelNum > 1) {
+        for (var i = 0; i < markerLabelNum - 1; i++) {
+            calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum - 1].getPosition());
+        }
+    }
+
+    var latlngX = document.getElementById("sel");
+    var option = document.createElement("option");
+    option.text = markers[markers.length - 1].getPosition();
+    latlngX.add(option);
+}
 
 function calculateAndDisplayRoute(x, y) {
     directionsService.route({
@@ -30,26 +53,32 @@ function calculateAndDisplayRoute(x, y) {
         destination: y,
         travelMode: 'WALKING'
     }, function(response, status) {
-        if (status === 'OK') {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (colorIndex === wow.length) {
+                colorIndex = 0;
+            }
             directionsDisplay = new google.maps.DirectionsRenderer({
                 polylineOptions: {
-                    strokeColor: wow[Math.floor(Math.random() * wow.length)]
+                    strokeColor: wow[colorIndex /*Math.floor(Math.random() * wow.length)*/ ]
                 }
             });
+            colorIndex++;
             directionsDisplay.setMap(map);
             directionsDisplay.setDirections(response);
+        } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+            window.alert('Developer Note: Directions request failed due to ' + status + '\n1 marker has 2 geocodes: lat ang lng\nAt 5 markers we are using 10 geocodes\nThe google maps api can do 10 per second');
         } else {
-            window.alert('Directions request failed due to ' + status);
+            window.alert('Developer Note: Directions request failed due to ' + status);
         }
     });
 }
 
 console.log(markers);
 
-function addMarker(x) {
+function addMarker(latlngX) {
     markers[markerLabelNum] = new google.maps.Marker({
         label: markerLabelNum.toString(),
-        position: x,
+        position: latlngX,
         map: map
     });
     markerLabelNum++;
@@ -59,31 +88,6 @@ function addMarker(x) {
 
 //deleteMarkers();
 
-function geocodeLatLng(geocoder, map) {
+/*function geocodeLatLng(geocoder, map) {
 
-}
-
-
-google.maps.event.addListener(map, "click", function(event) {
-    var latX = event.latLng.lat();
-    var lngX = event.latLng.lng();
-    var x = { lat: latX, lng: lngX };
-    addMarker(x);
-
-    if (markerLabelNum > 1) {
-        for (var i = 0; i < markerLabelNum - 1; i++) {
-            calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum - 1].getPosition());
-        }
-    }
-
-    geocodeLatLng(geocoder, map);
-
-    var x = document.getElementById("sel");
-    var option = document.createElement("option");
-    option.text = markers[markers.length - 1].getPosition();
-    x.add(option);
-
-    /*if (markers[markers.length - 1].getPosition().toString == "46.556618, 23.907623") {
-        alert("Sat de sapari...");
-    }*/
-});
+}*/
--- MAP/interface.js
+++ MAP/interface.js
@@ -10,3 +10,25 @@ function myFunction() {
     x.add(option);
     alert("Sat de sapari...");
 }
+
+/*google.maps.event.addListener(map, "click", function(event) {
+    var latX = event.latLng.lat();
+    var lngX = event.latLng.lng();
+    var x = { lat: latX, lng: lngX };
+    addMarker(x);
+
+    if (markerLabelNum > 1) {
+        for (var i = 0; i < markerLabelNum - 1; i++) {
+            calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum - 1].getPosition());
+        }
+    }
+
+    var x = document.getElementById("sel");
+    var option = document.createElement("option");
+    option.text = markers[markers.length - 1].getPosition();
+    x.add(option);
+
+    //if (markers[markers.length - 1].getPosition().toString == "46.556618, 23.907623") {
+    //    alert("Sat de sapari...");
+    //}
+});*/
