23ed466377a64672e2c81346ad8d2879670e239d
Szasz Imre <sz.lenard94@gmail.com>
2016-12-09 02:58:22 +0200

    Working on adding addresses to select.

--- MAP/googlemap.js
+++ MAP/googlemap.js
@@ -2,8 +2,10 @@ var markers = [];
 var markerLabelNum = 0;
 var responseIndex = 0;
 var responses = [];
+var myData = [];
+var wow = ["green", "purple", "red", "pink", "yellow", "blue", "orange", "white", "black", "brown"];
 
-var sarmasag = { lat: 47.3560031, lng: 22.8251987 };
+var latlngCenter = { lat: 45.7820031, lng: 25.0681987 };
 var cluj = { lat: 46.7833643, lng: 23.5464732 };
 var aghires = { lat: 47.1640112, lng: 23.004459 };
 
@@ -14,75 +16,90 @@ var directionsDisplay;
 function initMap() {
     directionsService = new google.maps.DirectionsService;
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: sarmasag
+        zoom: 7,
+        center: latlngCenter
     });
+}
 
-    function calculateAndDisplayRoute(x, y) {
-        directionsService.route({
-            origin: x,
-            destination: y,
-            travelMode: 'DRIVING'
-        }, function(response, status) {
-            if (status === 'OK') {
-                directionsDisplay = new google.maps.DirectionsRenderer;
-                directionsDisplay.setMap(map);
-                directionsDisplay.setDirections(response);
+// purpose: decode latitude and longitude to address
+var geocoder = new google.maps.Geocoder;
 
-                //responses[responseIndex] = response;
-                //responseIndex++;
-                //directionsDisplay.setDirections(responses[i]);
-                //console.log(response.routes[0].legs[0].distance.value);
-                //directionsDisplay.setDirections(responses[responseIndex]);
-                //console.log(response);
-            } else {
-                window.alert('Directions request failed due to ' + status);
-            }
-        });
-    }
-
-    //directionsService.route(requestArray[i], directionResults);
-
-    //addMarker(sarmasag);
-    //addMarker(aghires);
-    //addMarker(cluj);
-
-    console.log(markers);
-
-    //calculateAndDisplayRoute(markers[0].getPosition(), markers[1].getPosition());
-    //calculateAndDisplayRoute(markers[1].getPosition(), markers[2].getPosition());
-
-    //renderArray[cur] = new google.maps.DirectionsRenderer();
-    //renderArray[cur].setMap(map);
-    //renderArray[cur].setDirections(result);
-
-    function addMarker(x) {
-        markers[markerLabelNum] = new google.maps.Marker({
-            label: markerLabelNum.toString(),
-            position: x,
-            map: map
-        });
-        markerLabelNum++;
-    }
-
-    //pontokat tesz a mapra
-    //addMarker(aghires);
-    //utolsó marker törlése, tömbökkel bármelyik törölhető	
-    //markers.setMap(null);
-
-    //deleteMarkers();
-
-    google.maps.event.addListener(map, "click", function(event) {
-        var latX = event.latLng.lat();
-        var lngX = event.latLng.lng();
-        var x = { lat: latX, lng: lngX };
-        addMarker(x);
-
-        if (markerLabelNum > 1) {
-            for (var i = 0; i < markerLabelNum - 1; i++)
-                calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum - 1].getPosition());
+function calculateAndDisplayRoute(x, y) {
+    directionsService.route({
+        origin: x,
+        destination: y,
+        travelMode: 'WALKING'
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay = new google.maps.DirectionsRenderer({
+                polylineOptions: {
+                    strokeColor: wow[Math.floor(Math.random() * wow.length)]
+                }
+            });
+            directionsDisplay.setMap(map);
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
         }
     });
+}
 
+//directionsService.route(requestArray[i], directionResults);
+
+//addMarker(latlngCenter);
+//addMarker(aghires);
+//addMarker(cluj);
+
+console.log(markers);
+
+//calculateAndDisplayRoute(markers[0].getPosition(), markers[1].getPosition());
+//calculateAndDisplayRoute(markers[1].getPosition(), markers[2].getPosition());
+
+//renderArray[cur] = new google.maps.DirectionsRenderer();
+//renderArray[cur].setMap(map);
+//renderArray[cur].setDirections(result);
+
+function addMarker(x) {
+    markers[markerLabelNum] = new google.maps.Marker({
+        label: markerLabelNum.toString(),
+        position: x,
+        map: map
+    });
+    markerLabelNum++;
+}
+
+//pontokat tesz a mapra
+//addMarker(aghires);
+//utolsó marker törlése, tömbökkel bármelyik törölhető	
+//markers.setMap(null);
+
+//deleteMarkers();
+
+function geocodeLatLng(geocoder, map) {
 
 }
+
+
+google.maps.event.addListener(map, "click", function(event) {
+    var latX = event.latLng.lat();
+    var lngX = event.latLng.lng();
+    var x = { lat: latX, lng: lngX };
+    addMarker(x);
+
+    if (markerLabelNum > 1) {
+        for (var i = 0; i < markerLabelNum - 1; i++) {
+            calculateAndDisplayRoute(markers[i].getPosition(), markers[markerLabelNum - 1].getPosition());
+        }
+    }
+
+    geocodeLatLng(geocoder, map);
+
+    var x = document.getElementById("sel");
+    var option = document.createElement("option");
+    option.text = markers[markers.length - 1].getPosition();
+    x.add(option);
+
+    /*if (markers[markers.length - 1].getPosition().toString == "46.556618, 23.907623") {
+        alert("Sat de sapari...");
+    }*/
+});
--- MAP/interface.js
+++ MAP/interface.js
@@ -2,3 +2,11 @@ function autoSizeSelect() {
     var select = document.getElementById('sel');
     select.size = select.length + 1;
 }
+
+function myFunction() {
+    var x = document.getElementById("sel");
+    var option = document.createElement("option");
+    option.text = "Kiwi";
+    x.add(option);
+    alert("Sat de sapari...");
+}
