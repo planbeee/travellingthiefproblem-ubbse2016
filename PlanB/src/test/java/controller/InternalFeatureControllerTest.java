95801794153979e2dc0936c3cc8600480fa0efeb
robert.gyori <robert.gyori@yahoo.com>
2016-12-11 16:30:59 +0200

    Implement calls for preconditions and items.

---
+++ PlanB/src/test/java/controller/InternalFeatureControllerTest.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.apache.log4j.Logger;
+
+public class InternalFeatureControllerTest {
+
+    private Logger LOG = Logger.getLogger(InternalFeatureControllerTest.class.getName());
+
+    //TODO: robert.gyori write some tests
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # travellingthiefproblem-ubbse2016
 Travelling thief problem ubb 2016
+
+{"id": 123, ...}
